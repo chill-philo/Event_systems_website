02e063fc04a5065eed122bbe4e36f850a295b286
Joshua Kalule <kaljoshkalule@gmail.com>
2025-12-13 10:32:21 +0300

    add static file for eventsapp

--- Events/settings.py
+++ Events/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'eventsapp',  
+    'eventsapp',
 ]
 
 MIDDLEWARE = [
@@ -89,7 +89,9 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  
+STATICFILES_DIRS = [
+    BASE_DIR / 'eventsapp/static'
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
