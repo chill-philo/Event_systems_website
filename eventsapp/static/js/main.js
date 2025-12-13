a08fabead350cc74ce0dc8ed8a165d3440f80a8d
Musiime Philip <musiimephilip@gmail.com>
2025-12-13 10:44:32 +0300

    Added load static to html and changed products to services in work.

--- eventsapp/static/js/main.js
+++ eventsapp/static/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const productLinks = document.querySelectorAll(".product a");
+    const productLinks = document.querySelectorAll(".service a");
 
     productLinks.forEach(link => {
         link.addEventListener("click", function (event) {
