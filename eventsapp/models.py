1628365bd4041d889d88aa5cd573430ac2e4e783
Musiime Philip <musiimephilip@gmail.com>
2026-09-24 00:30:19 +0300

    Add event types

--- eventsapp/models.py
+++ eventsapp/models.py
@@ -21,6 +21,13 @@ class VendorProfile(models.Model):
     def __str__(self):
         return self.company_name
 
+class EventType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Vendor(models.Model):
     name = models.CharField(max_length=150)
     email = models.EmailField()
